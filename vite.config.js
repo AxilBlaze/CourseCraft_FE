5737d6973e08d826b691b29518dcd9018d883a74
AxilBlaze <sandeepcool2036@gmail.com>
2025-02-09 14:37:14 +0530

    favicon added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
-  
+  plugins: [react(), tailwindcss()],
+  publicDir: 'public',
 }); 
